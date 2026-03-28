f2ddef490a3af5866d094d0fb102f4d2f3547631
Yash Jangir <yashjangirg@gmail.com>
2026-03-28 16:32:01 +0530

    DbContext

---
+++ BuildChat/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuildChat.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+    }
+}
--- BuildChat/Program.cs
+++ BuildChat/Program.cs
@@ -1,5 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using BuildChat.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Add services to the container.
 
 builder.Services.AddControllers();
